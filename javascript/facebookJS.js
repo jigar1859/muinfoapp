0d43bb922648cffb771942adacd7a47ad75f7c7e
Jigar K. Thakkar <jigar1859@gmail.com>
2012-09-25 00:05:25 +0530

    Facebook SDK added

--- javascript/facebookJS.js
+++ javascript/facebookJS.js
@@ -1,60 +1,47 @@
-<!-- To bring in Facebook SDK-->
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '418666514860177', // App ID
-	  frictionlessRequests : true,
-      channelUrl : '//sultry-spire-1174.herokuapp.com/channel.php', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
-
-    // Additional initialization code here
-	
-	/*
-   Check the login staus of the user and authentication status of our app with respect to the user
-	Authorized implies we can access requested user data
- */
-FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    // the user is logged in and has authenticated your
-    // app, and response.authResponse supplies
-    // the user's ID, a valid access token, a signed
-    // request, and the time the access token 
-    // and signed request each expire
-    var uid = response.authResponse.userID;
-    var accessToken = response.authResponse.accessToken;
-	
-/* Implies User is logged in and has authorised the app to //access his data then do as desired in this section
-*/
-	
-	//we will user uid fetch the data and display on to the //console
-	
-	FB.api(uid, function(info){
-		console.log(info);
+	FB.init({
+		appId      : '418666514860177', // App ID
+		channelUrl : '//sultry-spire-1174.herokuapp.com/channel.php', // Channel File
+		status     : true, // check login status
+		cookie     : true, // enable cookies to allow the server to access the session
+		frictionlessRequests : true, // enable frictionless requests		
+		xfbml      : true  // parse XFBML
 	});
-	
-	
-  } else if (response.status === 'not_authorized') {
-    // the user is logged in to Facebook, 
-    // but has not authenticated your app
-	<!-- If user has not authorised us then redirect to the autho page -->
-    var oauth_url = 'https://www.facebook.com/dialog/oauth/';
-    oauth_url += '?client_id=418666514860177';
-    oauth_url += '&redirect_uri=' + encodeURIComponent('https://apps.facebook.com/YOUR_APP_NAMESPACE/');
-    oauth_url += '&scope=COMMA_SEPARATED_LIST_OF_PERMISSION_NAMES'
 
-  window.top.location = oauth_url;
-	
-  } else {
-    // the user isn't logged in to Facebook, redirect user to facebook login page
-	window.top.location="http://www.facebook.com/index.php";
-  }//response.status
- });//getLoginStatus
- 
-};//fbAsyncInit
+	// Additional initialization code here
 
- // Load the SDK Asynchronously
+	//Next, find out if the user is logged in
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			var uid = response.authResponse.userID;
+			accessToken = response.authResponse.accessToken;
+			
+			FB.api('/me', function(info) {
+				console.log(info);
+  			  	console.log(first_name);
+			});
+
+
+		} else if (response.status === 'not_authorized') {
+			//User is logged into Facebook, but not your App
+
+
+			  var oauth_url = 'https://www.facebook.com/dialog/oauth/';
+			  oauth_url += '?client_id=401236519908500'; //Your Client ID
+			  oauth_url += '&redirect_uri=' + 'https://apps.facebook.com/muinfoapp/'; //Send them here if they're not logged in
+			  oauth_url += '&scope=user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes';
+		
+			  window.top.location = oauth_url;
+
+
+		} else {
+			// User is not logged into Facebook at all
+			window.top.location ='https://www.facebook.com/index.php';
+		} //response.status
+	}); //getLoginStatus
+}; //fbAsyncInit
+
+// Load the SDK Asynchronously
   (function(d){
      var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
      if (d.getElementById(id)) {return;}
