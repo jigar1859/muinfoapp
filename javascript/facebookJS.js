652f1ba7fe0e15991f944dfd1dbe98ff1f343d00
Jigar K. Thakkar <jigar1859@gmail.com>
2012-09-24 20:27:21 +0530

    facebook sdk code

---
+++ javascript/facebookJS.js
@@ -0,0 +1,24 @@
+// Facebook JavaScript Document
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '418666514860177', // App ID
+	  frictionlessRequests : true,
+      channelUrl : '//sultry-spire-1174.herokuapp.com/channel.php', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
